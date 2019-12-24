f2e0840a40f677e340df6b4d1e798601fddd8f67
Joseph BERTHE Desktop <joseph.berthe@gmail.com>
2019-12-24 15:57:53 +0100

    add doc in cli + file as arg

--- ui5-bump-version.js
+++ ui5-bump-version.js
@@ -11,33 +11,26 @@ const tArguments = ['major', 'minor', 'patch', 'build'];
 // Get arguments Level
 console.log(path.dirname(__filename));
 if (process.argv[2]) {
-    let level = process.argv[2];
+  let level = process.argv[2];
 
-    if (bump.isArgumentAvailable(level)) {
-    //  console.log(path.join(path.dirname(__filename)),"webapp",  file);
-        let oData = jsonfile.readFileSync(file);
-        let oldVersion = bump.readApplicationVersion(oData);
-        let newVersion = bump.bumpVersion(oldVersion, tArguments.indexOf(level));
-        let newData = bump.appendNewVersion(oData, newVersion);
+  if (bump.isArgumentAvailable(level)) {
+    try {
+      let oData = jsonfile.readFileSync(process.argv[3] || file);
+      let oldVersion = bump.readApplicationVersion(oData);
+      let newVersion = bump.bumpVersion(oldVersion, tArguments.indexOf(level));
+      let newData = bump.appendNewVersion(oData, newVersion);
 
-        jsonfile.writeFile(file, newData, function (err) {
-            if (err) console.error(err)
-          });
-
-    } else {
-        console.error("Arguments not available.");
+      jsonfile.writeFile(file, newData, function (err) {
+        if (err) console.error(err)
+      });
+    } catch (error) {
+      console.error("File " + process.argv[3] + " not found.");
     }
+  } else {
+    console.error("Arguments not available.");
+  }
 
+} else {
+  console.log("\nUsage: ui5-bump {major|minor|patch|build} |path|\n");
+  console.log("\Where: path stand for the full path to manifest.json. By default path is webapp/manifest.json\n");
 }
-
-
-
-
-
-
-/* console.info("file read: " + currentFile);
- console.dir(jsonfile.readFileSync(currentFile));
- if(currentObj){
-   console.info("file read");
-   console.dir(jsonfile.readFileSync(currentFile));
- }*/
